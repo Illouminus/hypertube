3feeb4699a334694664be01cc057cbab5d299f50
Illouminus <professionnel.baillot@gmail.com>
2026-03-12 15:24:28 +0100

    feat: add Prisma, Users, and Auth modules with corresponding services and controllers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -9,6 +12,9 @@ import { AppService } from './app.service';
       envFilePath: '../.env',
       isGlobal: true,
     }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
